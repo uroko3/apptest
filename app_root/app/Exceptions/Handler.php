0bc1bc8672c2ed1e5719f900a311d20b39ad4945
uroko <uroko@example.com>
2024-04-14 15:21:13 +0900

    5to9

--- app_root/app/Exceptions/Handler.php
+++ app_root/app/Exceptions/Handler.php
@@ -2,52 +2,49 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
+	/**
+	 * A list of exception types with their corresponding custom log levels.
+	 *
+	 * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+	 */
+	protected $levels = [
+		//
+	];
+	
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array<int, class-string<\Throwable>>
+	 */
+	protected $dontReport = [
+		//
+	];
+	
+	/**
+	 * A list of the inputs that are never flashed to the session on validation exceptions.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $dontFlash = [
+		'current_password',
+		'password',
+		'password_confirmation',
+	];
+	
+	/**
+	 * Register the exception handling callbacks for the application.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->reportable(function (Throwable $e) {
+			//
+		});
+	}
 }
