79c26b01141c3678d756c0d8ca2ce80001e8b8be
uroko <uroko@example.com>
2024-04-20 09:20:46 +0900

    sql log add

--- app_root/config/app.php
+++ app_root/config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+    	
+    	App\Providers\DatabaseQueryServiceProvider::class,
 
         /*
          * Package Service Providers...
