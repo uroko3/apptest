75162997bc1798eae02f851fb19c60fd11ce92cf
uroko <uroko@example.com>
2024-05-07 21:48:26 +0900

    domain routes

--- app_root/routes/web.php
+++ app_root/routes/web.php
@@ -11,34 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+
+Route::domain('apptest.example.com')->group(function () {
+	Route::get('/', function () {
+	    return view('home');
+	});
+	
+	Route::get('/home', function () {
+		return view('home');
+	})->middleware(['home']);
+	
+	Route::get('/auth/register', 'Auth\RegisterController@showRegistrationForm');
+	Route::post('/auth/register', 'Auth\RegisterController@register');
+	
+	Route::get('/auth/login', 'Auth\LoginController@showLoginForm');
+	Route::post('/auth/login', 'Auth\LoginController@login');
+	Route::get('/auth/logout', 'Auth\LoginController@logout');
+	
+	Route::get('/form', 'TestController@form');
+	Route::post('/form', 'TestController@confirm');
+	Route::post('/form/complete', 'TestController@complete');
+	
+	Route::group(['prefix' => 'gitlab'], function(){
+		Route::get('', 'GitlabController@index')->name('gitlab');
+		Route::get('oauth', 'GitlabController@oauth')->name('aouth');
+		Route::get('ggg', 'GitlabController@ggg');
+		Route::get('password', 'GitlabController@password');
+	});
+
+	Route::get('/form2', 'FormController@index');
+	Route::post('/form2', 'FormController@postValidates');
 });
 
 
-Route::get('/home', function () {
-	return view('home');
-})->middleware(['home']);
-
-Route::get('/auth/register', 'Auth\RegisterController@showRegistrationForm');
-Route::post('/auth/register', 'Auth\RegisterController@register');
-
-Route::get('/auth/login', 'Auth\LoginController@showLoginForm');
-Route::post('/auth/login', 'Auth\LoginController@login');
-Route::get('/auth/logout', 'Auth\LoginController@logout');
-
-Route::get('/form', 'TestController@form');
-Route::post('/form', 'TestController@confirm');
-Route::post('/form/complete', 'TestController@complete');
-
-Route::group(['prefix' => 'gitlab'], function(){
-	Route::get('', 'GitlabController@index')->name('gitlab');
-	Route::get('oauth', 'GitlabController@oauth')->name('aouth');
-	Route::get('ggg', 'GitlabController@ggg');
-	Route::get('password', 'GitlabController@password');
+Route::domain('hoge.example.com')->group(function () {
+	Route::get('/', function () {
+		dd('hoge route');
+	});
 });
-
-Route::get('/form2', 'FormController@index');
-Route::post('/form2', 'FormController@postValidates');
-
-
